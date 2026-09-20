2874150bdbc6e4b5c1ba951d746f57ed7f37b5e9
Suvram <67403378+suvram-code@users.noreply.github.com>
2026-09-20 13:39:04 +0530

    Add: level-order-traversal-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-0.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> v;
+        if(!root) return v;
+        std::stack<TreeNode*> s;
+        TreeNode* r=root;
+        s.push(r);
+        TreeNode* jp=root; //just popped treendoe
+        int depth=0;
+        int size=0;
+        
+        
+        while(!s.empty()){
+            r=s.top();
+            
+            if( r->left!=jp && r->right!=jp){
+                //juste netered
+                 depth++;
+                 if(depth>size){
+                    vector<int> temp;
+                    v.push_back(temp);
+                    size++;
+                 }
+                 v[depth-1].push_back(r->val);
+
+                if(r->left) s.push(r->left);
+                else if(r->right) s.push(r->right);
+                else {
+                    //pop it, go up
+                    jp=r;
+                    s.pop();
+                    depth--;
+                }
+            }else if(jp==r->left){
+                //entered from right side
+                if(r->right) s.push(r->right);
+                else{
+                    //pop it, go up
+                    jp=r;
+                    s.pop();
+                    depth--;
+                }
+            }else if(jp==r->right){
+                jp=r;
+                s.pop();
+                depth--;
+            }
+        }
+
+        return v;
+
+    }
+};
